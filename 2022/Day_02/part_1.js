d2c8ce286a03193bf7e49b00af21c9cc4ce2665f
arellak <nimble0@protonmail.com>
2022-12-02 13:15:14 +0100

    day two

---
+++ 2022/Day_02/part_1.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const fs = require("fs");
+const path = require("path");
+const { performance } = require("perf_hooks");
+
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).split("\n"); // change this if necessary
+
+const pStart = performance.now();
+
+let score = 0;
+
+INPUT.map(line => line.split(" ")).forEach(line => {
+    score += line[1] === 'X' ? 1 : line[1] === 'Y' ? 2 : line[1] === 'Z' ? 3 : 0;
+
+    if((line[0] === 'A' && line[1] === 'X') || (line[0] === 'B' && line[1] === 'Y') || (line[0] === 'C' && line[1] === 'Z')) {
+        score += 3;
+    } else if((line[0] === 'A' && line[1] === 'Y') || (line[0] === 'B' && line[1] === 'Z') || (line[0] === 'C' && line[1] === 'X')) {
+        score += 6;
+    }
+});
+
+const result = score;
+
+const pEnd = performance.now();
+
+console.log("Rock Paper Scissors Part 1: " + result);
+console.log(pEnd - pStart);
---
+++ 2022/Day_02/part_2.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const fs = require("fs");
+const path = require("path");
+const { performance } = require("perf_hooks");
+
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).split("\n"); // change this if necessary
+
+const pStart = performance.now();
+
+let score = 0;
+
+INPUT.map(line => line.split(' ')).forEach(line => {
+    score += line[1] === 'X' ? line[0] === 'A' ? 3 : line[0] === 'B' ? 1 : line[0] === 'C' ? 2 : 0
+            : line[1] === 'Y' ? 3 + (line[0] === 'A' ? 1 : line[0] === 'B' ? 2 : line[0] === 'C' ? 3 : 0)
+            : line[1] === 'Z' ? 6 + (line[0] === 'A' ? 2 : line[0] === 'B' ? 3 : line[0] === 'C' ? 1 : 0) : 0;
+});
+
+const result = score;
+
+const pEnd = performance.now();
+
+console.log("Rock Paper Scissors Part 2: " + result);
+console.log(pEnd - pStart);
